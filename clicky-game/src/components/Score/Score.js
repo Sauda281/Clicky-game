c0ef23be09ba540e5b922ffb3288fbb2b79a7dbb
Sauda281 <58537112+Sauda281@users.noreply.github.com>
2020-05-28 23:33:24 -0500

    styling

--- clicky-game/src/components/Score/Score.js
+++ clicky-game/src/components/Score/Score.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Score.css";
-import Arrow from 'react-icons/lib/fa/caret-right';
+// import Arrow from 'react-icons/lib/fa/caret-right';
+import { FaBeer } from 'react-icons/fa';
 
 const Score = props => (
     <div className ="gameScore">
-        <h3 className="score">Your Score <Arrow /> {props.total}</h3>
+        <h3 className="score">Your Score <FaBeer /> {props.total}</h3>
         <h3 className="status">{props.status}</h3>
     </div>
 );
